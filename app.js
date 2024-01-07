310efd0268e7a08e8988daee47ac1ddf30040803
worldwilddev <worldwilddev@dev.com>
2024-01-08 04:45:15 +0700

    feat: changeStatus

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
-const mySwitch = require('./app.json');
+// const mySwitch = require('./app.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const app = express();
 const port = 3000;
 
-console.log('My Switch:', mySwitch);
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
 
 
 app.get('/status', (req, res) => {
+  const mySwitch = JSON.parse(fs.readFileSync('app.json', 'utf-8'));
+  console.log('[ get status ]', req.ip, req.get('User-Agent'));
   if (mySwitch.status == 'prod') {
     res.status(200).send(true);
   } else {
     res.status(400).send(false);
   }
+})
+
+app.post('/changeStatus', (req, res) => {
+  const requestBody = req.body;
+  // console.log(requestBody)
+  // console.log(requestBody.status)
+  if (requestBody.status == 'prod') {
+    fs.writeFile('app.json', JSON.stringify({status: "prod"}, null, 4), 'utf8', (err) => {
+    if (err) throw err;
+    console.log('app.json has been updated');
+  });
+  }
+  else {
+    fs.writeFile('app.json', JSON.stringify({status: "notprod"}, null, 4), 'utf8', (err) => {
+    if (err) throw err;
+    console.log('app.json has been updated');
+  });
+  }
+  res.status(200).send('OKAY');
 })
 // Route to handle POST requests to /echo
 app.post('/echo', (req, res) => {
