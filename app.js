edec0b270bc15a1cd0ab872938653a81016683f3
worldwilddev <worldwilddev@dev.com>
2024-02-03 19:12:36 +0700

    anypayV0:

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON request bodies
 app.use(bodyParser.json());
+
+app.get('/logger', (req, res) => {
+    const filePath = path.join(__dirname, 'log.txt'); // Path to log.txt in the current directory
+
+    // Check if the file exists
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        if (err) {
+            // File does not exist
+            res.status(404).send('Log file not found.');
+        } else {
+            // File exists, set appropriate headers for download
+            res.setHeader('Content-Disposition', 'attachment; filename=log.txt');
+            res.setHeader('Content-Type', 'text/plain');
+
+            // Stream the file to the response
+            const fileStream = fs.createReadStream(filePath);
+            fileStream.pipe(res);
+        }
+    });
+});
 
 // Route to handle POST requests to /echo
 app.post('/echo', (req, res) => {
